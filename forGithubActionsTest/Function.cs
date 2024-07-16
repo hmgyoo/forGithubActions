a599c87c17028945691ad4564f0ad7d19c000e55
Gary Daniel <ernogarydaniel@gmail.com>
2024-07-16 13:22:53 +0800

    Update Function.cs

--- forGithubActionsTest/Function.cs
+++ forGithubActionsTest/Function.cs
@@ -14,6 +14,7 @@ public class Function
     /// <param name="input">The event for the Lambda function handler to process.</param>
     /// <param name="context">The ILambdaContext that provides methods for logging and describing the Lambda environment.</param>
     /// <returns></returns>
+    // tseaargargaergaerg
     public string FunctionHandler(string input, ILambdaContext context)
     {
         return input.ToUpper();
